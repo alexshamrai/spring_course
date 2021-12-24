150ebce23b6662f088fcb32e49c14ce0b3c759c4
Oleksii Shamrai <oshamrai@playtika.com>
2021-12-24 14:54:10 +0200

    Chapter#1: IoC & DI

--- src/main/java/org/example/spring_introduction/Test1.java
+++ src/main/java/org/example/spring_introduction/Test1.java
@@ -2,7 +2,7 @@ package org.example.spring_introduction;
 
 public class Test1 {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.say();
+        Pet pet = new Cat();
+        pet.say();
     }
 }
