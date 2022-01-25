d4be75eee5a2eaf0bb383ff47ca31974a4a1f58a
Oleksii Shamrai <oshamrai@playtika.com>
2022-01-25 10:28:53 +0200

    Chapter#1 @Scope, @PostConstruct, @PreDestroy

--- src/main/java/org/example/spring_introduction/Dog.java
+++ src/main/java/org/example/spring_introduction/Dog.java
@@ -1,8 +1,13 @@
 package org.example.spring_introduction;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class Dog implements Pet {
 
 //    private String name;
@@ -15,10 +20,12 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("Class Dog: init method");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("Class Dog: destroy method");
     }
