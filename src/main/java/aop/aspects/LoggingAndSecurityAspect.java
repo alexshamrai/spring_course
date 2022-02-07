92d20b4fef7e4d9080c1de5d8fd1eb0d7ca25db3
Oleksii Shamrai <oshamrai@playtika.com>
2022-02-07 18:13:44 +0200

    Declaration of the Pointcut

---
+++ src/main/java/aop/aspects/LoggingAndSecurityAspect.java
@@ -0,0 +1,24 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAndSecurityAspect {
+
+    @Pointcut("execution(* get*())")
+    private void allGetMethods(){};
+
+    @Before("allGetMethods()")
+    public void beforeGetBookAdvice() {
+        System.out.println("beforeGetBookAdvice: Trying to get a book/magazine");
+    }
+
+    @Before("allGetMethods()")
+    public void beforeGetSecurityAdvice(){
+        System.out.println("beforeGetSecurityAdvice: check rights for getting a book/magazine");
+    }
+}
