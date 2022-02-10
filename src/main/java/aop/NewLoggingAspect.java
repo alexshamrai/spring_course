69d9af0e8df335a0560b2c2f73864324ba35624b
Oleksii Shamrai <oshamrai@playtika.com>
2022-02-10 17:33:48 +0200

    Chapter#2 - Advice type @Around: working with Exceptions

--- src/main/java/aop/NewLoggingAspect.java
+++ src/main/java/aop/NewLoggingAspect.java
@@ -13,13 +13,16 @@ public class NewLoggingAspect {
     public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice: a try to return book to the Library");
 
-        long begin = System.currentTimeMillis();
-        Object targetMethodResult = proceedingJoinPoint.proceed();
-//        targetMethodResult = "Harry Potter";
-        long end = System.currentTimeMillis();
+        Object targetMethodResult = null;
+        try {
+             targetMethodResult = proceedingJoinPoint.proceed();
+        }
+        catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: exception was logged " + e);
+            throw e;
+        }
 
         System.out.println("aroundReturnBookLoggingAdvice: book is returned successfully to the Library");
-        System.out.println("aroundReturnBookLoggingAdvice: method returnBook time execution " + (end-begin) + " ms.");
         return targetMethodResult;
     }
 }
--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -11,6 +11,7 @@ public class UniLibrary extends AbstractLibrary {
     }
 
     public String returnBook() {
+        int a = 10/0;
         System.out.println("We are returning a book to UniLibrary");
         return "flying over cuckoo's nest";
     }
